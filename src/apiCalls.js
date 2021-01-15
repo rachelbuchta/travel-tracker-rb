05d06c9ebf53f6132ffc4910eb636ff2029d8eb4
Rachel Buchta <rachelbuchta@yahoo.com>
2021-01-15 11:00:50 -0700

    Get API calls set up and connected to scripts

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,18 +1,35 @@
 const fetchCalls = {
 
   getTraveler(id) {
-    return fetch()
+    return fetch(`http://localhost:3001/api/v1/travelers/${id}`)
     .then(response => response.json())
+    .catch(err => {
+      alert("Sorry! We are having trouble getting the data, try again later!")
+    })
+  },
+
+  getAllTravelers() {
+    return fetch("http://localhost:3001/api/v1/travelers")
+    .then(response => response.json())
+    .catch(err => {
+      alert("Sorry! We are having trouble getting the data, try again later!")
+    })
   },
 
   getTrips() {
-    return fetch()
+    return fetch("http://localhost:3001/api/v1/trips")
     .then(response => response.json())
+    .catch(err => {
+      alert("Sorry! We are having trouble getting the data, try again later!")
+    })
   },
 
   getDestinations() {
-    return fetch()
+    return fetch("http://localhost:3001/api/v1/destinations")
     .then(response => response.json())
+    .catch(err => {
+      alert("Sorry! We are having trouble getting the data, try again later!")
+    })
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
+import Traveler from './traveler.js'
+import Trip from './trip.js'
+import Destination from './destination.js'
+
+import fetchCalls from './apiCalls'
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
-import '.traveler.js'
+// import '.traveler.js'
 
-let traveler = new Traveler
+let traveler;
+let trip;
+let destination;
+let travelerData;
+let allTravelersData;
+let tripsData;
+let destinationsData;
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+
+
+const initiateData = () => {
+  console.log("HI")
+  const data = Promise.all([fetchCalls.getTraveler(1), fetchCalls.getTrips(), fetchCalls.getDestinations()])
+  .then(data => {
+    traveler = new Traveler(data[0]);
+    console.log(trip = new Trip(data[1]));
+    destination = new Destination(data[2]);
+  })
+console.log(data.traveler)
+return data
+}
+window.addEventListener('load', initiateData)
+
