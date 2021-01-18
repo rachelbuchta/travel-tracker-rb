7ec67d6b42491c07e8b45cffb078c2acc1d031c7
Rachel Buchta <rachelbuchta@yahoo.com>
2021-01-18 10:40:04 -0700

    Build booking post method

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -31,6 +31,38 @@ const fetchCalls = {
         alert("Sorry! We are having trouble getting the data, try again later!")
       })
   }
+
+  postTrip() {
+    let newTrip = fetchAPI.buildTripObject(trip);
+    let postedData = fetch('http://localhost:3001/api/v1/trips', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newBooking)
+    })
+      .then(response => response.json())
+      .catch(error => {
+        alert("Sorry, We are having trouble getting the data, try again later!")
+      })
+      return postedData
+  },
+
+  buildTripObject(booking) {
+    let bookingObject = {
+      id: trip.id,
+      userID: trip.userID,
+      destinationID: trip.destinationID,
+      travelers: trip.travelers,
+      date: trip.date,
+      duration: trip.duration,
+      status: trip.status,
+      suggestedActivities: trip.suggestedActivities,
+    }
+    return bookingObject
+  }
+
+
 }
 
 export default fetchCalls;
